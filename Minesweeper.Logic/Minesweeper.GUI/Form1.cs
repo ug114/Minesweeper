133f3a62503a9d86e117f140824383e2ac9d1f3c
user <user@WIN-2KR6UHLB0NV>
2018-08-23 10:04:58 +0400

    Two projects added

--- Minesweeper.Logic/Minesweeper.GUI/Form1.cs
+++ Minesweeper.Logic/Minesweeper.GUI/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Minesweeper.Logic
+namespace Minesweeper.GUI
 {
     public partial class Form1 : Form
     {
--- Minesweeper.TextUI/Form1.Designer.cs
+++ Minesweeper.TextUI/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Minesweeper.Logic
+﻿namespace Minesweeper.TextUI
 {
     partial class Form1
     {
@@ -28,17 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "  V";
-            this.ResumeLayout(false);
-
+            this.Text = "Form1";
         }
 
         #endregion
