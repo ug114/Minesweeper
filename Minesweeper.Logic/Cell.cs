5f97d5b4866147c693af3b176b90a0150abecdcc
user <user@WIN-2KR6UHLB0NV>
2018-08-23 10:18:00 +0400

    Box and Cell classes added

---
+++ Minesweeper.Logic/Cell.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper.Logic
+{
+    public class Cell
+    {
+        public int x;
+        public int y;
+        public string state;
+    }
+}
---
+++ Minesweeper.TextUI/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper.TextUI
+{
+    public class Class1
+    {
+        public void GetCell()
+        {
+            Console.WriteLine("Enter x: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
